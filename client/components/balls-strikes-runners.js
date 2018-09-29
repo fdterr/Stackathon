9d970b7e2ea7d5b815dfce430b0c5695d42f18c5
Rick Terry <fdterr@gmail.com>
2018-09-29 14:12:28 -0400

    bases, count, outs reformatted, batter and pitcher added

--- client/components/balls-strikes-runners.js
+++ client/components/balls-strikes-runners.js
@@ -3,17 +3,25 @@ import React from 'react';
 export default function BallStrikesRunners(props) {
   return (
     <div className="balls-strikes-runners">
-      <div className="balls-strikes">
-        {props.balls}
-        {'\u2014'}
-        {props.strikes} {/** '\u2014' is an mdash (&mdash) */}
+      <div className="balls-strikes-outs">
+        <div className="count">
+          {props.balls}
+          {' \u2014 '}
+          {props.strikes}
+        </div>
+
+        <div className="outs">Outs: {props.outs}</div>
       </div>
-      <div className="outs">Outs: {props.outs || '\u2014'}</div>
+
       <div className="runners">
         <div className="base" />
         <div className="base" />
         <div className="base" />
         <div className="base home" />
+      </div>
+
+      <div className="pitcher-hitter">
+        P: David Wright <br /> B: David Wright
       </div>
     </div>
   );
